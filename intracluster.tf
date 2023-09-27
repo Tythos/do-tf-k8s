1e746349001782bb6fce4ca4608aa083ce1ef5e9
Brian Kirkpatrick <code@tythos.net>
2023-09-26 18:19:44 -0700

    interim commit while switching from gpt3.5 to gpt4

--- intracluster.tf
+++ intracluster.tf
@@ -51,3 +51,67 @@ resource "kubernetes_deployment" "nginx_deployment" {
     }
   }
 }
+
+resource "kubernetes_deployment" "nginx_ingress_controller" {
+  metadata {
+    name = "nginx-ingress-controller"
+    labels = {
+      app = "ingress-nginx"
+    }
+  }
+
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        app = "ingress-nginx"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "ingress-nginx"
+        }
+      }
+      spec {
+        container {
+          name  = "nginx-ingress-controller"
+          image = "quay.io/kubernetes-ingress-controller/nginx-ingress-controller:0.27.1" # Replace with the desired NGINX Ingress Controller image version
+          ports {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "nginx_ingress_controller" {
+  metadata {
+    name = "nginx-ingress-controller"
+    labels = {
+      app = "ingress-nginx"
+    }
+  }
+
+  spec {
+    selector = {
+      app = "ingress-nginx"
+    }
+    ports {
+      port        = 80
+      target_port = 80
+    }
+    type = "LoadBalancer" # Use "LoadBalancer" type if you want to expose the Ingress controller externally
+  }
+}
+
+resource "kubernetes_config_map" "nginx_ingress_config" {
+  metadata {
+    name = "nginx-config"
+  }
+
+  data = {
+    # Add your NGINX Ingress configuration here (e.g., custom error pages, proxy-buffer-size, etc.)
+  }
+}
